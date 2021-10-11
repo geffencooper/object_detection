fixed model via normalization

src/network_def.py
@@ -7,12 +7,45 @@ This file defines multiple CNN models for object classification/detection
 import torch
 #from torch.cuda import set_device
 import torch.nn as nn
-import torch.nn.Functional as F
+import torch.nn.functional as F
 #import torch.nn.utils.rnn as rnn_utils
 #import copy 
+torch.manual_seed(42)
 
-# This is the base CNN used to train an object classifier (images 128x128)
-# The convolution layers are taken from this model for object detection
+# ===================================================================================================== #
+# ============================================= MODELS ================================================ #
+# ===================================================================================================== #
+'''
+MNIST CNN used to make sure training is working correctly
+Parameters:
+    None
+'''
+class MNISTClassifier(nn.Module):
+    def __init__(self):
+        super(MNISTClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+
+# --------------------------------------------------------------------------------- #
+
+'''
+Base CNN used for object classification (images 128x128)
+Parameters:
+    args - these are the args specified in the config file and are passed
+           in by the training function in network_train.py
+'''
 class ObjectClassifier128(torch.nn.Module):
     def __init__(self,args):
         super(ObjectClassifier128,self).__init__()
@@ -87,19 +120,12 @@ class ObjectClassifier128(torch.nn.Module):
         x = self.pool(x)
         x = F.relu(self.conv10(x))
         
+        # flatten the batch_size x 64x4x4 tensor to be batch_size x (64*4*4)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         
         return x
-
-    # initialize the hidden state at the start of each forward pass
-    def init_hidden(self,batch_size):
-        if self.init == True:
-            self.h0 = torch.randn(self.num_layers,batch_size,self.hidden_size)
-        else:
-            self.h0 = torch.zeros(self.num_layers,batch_size,self.hidden_size)
-        self.h0 = self.h0.to(self.device)
 
 
 

src/pytorch_dataset.py
@@ -20,10 +20,12 @@ import torch
 import pandas as pd
 import cv2
 import random
-
+import warnings
+#warnings.filterwarnings("error")
+torch.manual_seed(42)
 
 # ===================================================================================================== #
-# ============================================= CLASSES =============================================== #
+# ========================================= DATASET CLASSES =========================================== #
 # ===================================================================================================== #
 '''
 Generic Class for creating object classifier datasets
@@ -37,36 +39,53 @@ Parameters:
   transform -    Specifies the image format (size, RGB, etc.) and augmentations to use
 '''
 class ObjectClassifierDataset(Dataset):
-    def __init__(self,img_dir_path,transform):
+    def __init__(self,img_dir_path,transform,normalize):
         self.img_dir_path = img_dir_path
         self.transform = transform
+        self.normalize = normalize
         
         # collect img classes from dir names
         img_classes = next(os.walk(img_dir_path))[1]
         
         # generate a dictionary to map class names to integers idxs
-        self.labels = {img_classes[i] : i for i in range(0, len(img_classes))}
+        self.classes = {img_classes[i] : i for i in range(0, len(img_classes))}
         
-        # get all training samples by getting paths of the images in each subfolder folder
+        # get all training samples/labels by getting paths of the images in each subfolder folder
         self.imgs = []
+        self.labels = []
+        i = 0
         for idx, path_obj in enumerate(os.walk(img_dir_path)):
             if idx > 0: # we don't want the files in the top folder
                 for file in path_obj[2]: # path_obj[2] is list of files in the subdirectory
-                    self.imgs.append(os.path.abspath(os.path.join(path_obj[0],file))) # want absolute path 
+                    self.imgs.append(os.path.abspath(os.path.join(path_obj[0],file))) # want absolute path
+                    self.labels.append(self.classes[os.path.basename(os.path.dirname(self.imgs[i]))]) # get the label from the directory name
+                    i+=1
     
     def __len__(self):
         return len(self.imgs)
     
     def __getitem__(self, idx):
         # load the image
-        img = io.imread(self.imgs[idx])
-        
-        # apply any transformation
-        if self.transform:
-            img = self.transform(img)
-        
-        # return the sample (img (tensor)), object class (int), sample index (int))
-        return img, self.labels[os.path.basename(os.path.dirname(self.imgs[idx]))], idx
+        try:
+            img = io.imread(self.imgs[idx])
+            
+            # apply any transformation
+            if self.transform:
+                img = self.transform(img)
+                if self.normalize:
+                    norm = torchvision.transforms.Normalize((torch.mean(img)),(torch.std(img)))
+                    img = norm(img)
+                    
+            
+            # get the label
+            label = self.labels[idx]
+            
+            # return the sample (img (tensor)), object class (int), sample index (int))
+            return img, label, idx
+        except (ValueError, RuntimeWarning,UserWarning) as e:
+            print("Exception: ", e)
+            print("Bad Image: ", self.imgs[idx])
+            exit()
     
     # Displays a random batch of 64 samples
     def visualize_batch(self):
@@ -79,7 +98,7 @@ class ObjectClassifierDataset(Dataset):
         # display the batch in a grid with the img, label, idx
         rows = 8
         cols = 8
-        obj_classes = list(self.labels)
+        obj_classes = list(self.classes)
         
         fig,ax_array = plt.subplots(8,8,figsize=(20,20))
         fig.subplots_adjust(hspace=0.5)
@@ -94,6 +113,57 @@ class ObjectClassifierDataset(Dataset):
         plt.show()
 
 
+# ===================================================================================================== #
+# ====================================== CREATE DATASET FUNCTIONS ===================================== #
+# ===================================================================================================== #
+def create_MNIST_dataset():
+    train_dataset = torchvision.datasets.MNIST('/home/geffen/Desktop/MNIST/', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ]))
+    test_dataset = torchvision.datasets.MNIST('/home/geffen/Desktop/MNIST/', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ]))
+    
+    return train_dataset,test_dataset
+
+# ------------------------------------------------------------------------------------------------ #
+
+def create_ObjectClassifier128_dataset(normalize):
+    # determine the data transformations and format
+    train_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((128, 128)),
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor(),
+            transforms.RandomHorizontalFlip(),
+            torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,)),
+            transforms.RandomAffine(degrees=5,scale=(0.45,1.25),translate=(0.3,0.3))
+        ])
+    
+    test_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((128, 128)),
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+        ])
+    
+    img_dir_path = "/home/geffen/Desktop/Face_Detector/assemble_face_dataset_utils/face_classifier_dataset"
+    
+    # create the datasets
+    train_dataset = ObjectClassifierDataset(os.path.join(img_dir_path,"train"),train_transform,normalize)
+    test_dataset = ObjectClassifierDataset(os.path.join(img_dir_path,"test"),test_transform,normalize)
+    
+    return train_dataset, test_dataset
+
 
 # ===================================================================================================== #
 # ============================================= MAIN ================================================== #
@@ -101,11 +171,17 @@ class ObjectClassifierDataset(Dataset):
 
 if __name__  == "__main__":
     # define training data format
-    test_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((128, 128)),
-            transforms.Grayscale(num_output_channels=1)
-        ])
-    img_data_dir1 = "/home/geffen/Desktop/Face_Detector/assemble_face_dataset_utils/face_classifier_dataset/test"
-    faces_dataset = ObjectClassifierDataset(img_data_dir1,test_transform)
-    faces_dataset.visualize_batch()
+    # test_transform = transforms.Compose([
+    #         transforms.ToPILImage(),
+    #         transforms.Resize((128, 128)),
+    #         transforms.Grayscale(num_output_channels=1),
+    #         transforms.ToTensor(),
+    #     ])
+    # img_data_dir1 = "/home/geffen/Desktop/Face_Detector/assemble_face_dataset_utils/face_classifier_dataset/test"
+    # faces_dataset = ObjectClassifierDataset(img_data_dir1,test_transform)
+    # faces_dataset.visualize_batch()
+    dataset = create_ObjectClassifier128_dataset(True)[0]
+    dataset.visualize_batch()
+    # loader = DataLoader(dataset,batch_size=64)
+    # for i,batch in enumerate(loader):
+    #     print(i)